ba4629a7811e992dda433a1a9d6110115a6af9da
calvin-zcx <chengxi.zang@gmail.com>
2022-08-03 00:48:26 -0400

    lstm model

---
+++ main_lstm.py
@@ -0,0 +1,283 @@
+import sys
+
+# for linux env.
+sys.path.insert(0, '..')
+import time
+import pickle
+import argparse
+
+from supp_utils import *
+from torch.utils.data.sampler import SubsetRandomSampler
+import numpy as np
+import torch
+import torch.nn.functional as F
+import random
+import pandas as pd
+import json
+import matplotlib.pyplot as plt
+from model import lstm, ml
+import itertools
+import functools
+
+print = functools.partial(print, flush=True)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='process parameters')
+    # Input
+    parser.add_argument('--data_dir', type=str, default='../ipreprocess/output/save_cohort_all/')
+    # parser.add_argument('--treated_drug', type=str)
+    # parser.add_argument('--controlled_drug', choices=['atc', 'random'], default='random')
+    # parser.add_argument('--controlled_drug_ratio', type=int, default=3)  # 2 seems not good. keep unchanged
+    parser.add_argument("--random_seed", type=int, default=0)
+
+    # parser.add_argument('--run_model', choices=['LSTM', 'LR', 'MLP', 'XGBOOST', 'LIGHTGBM'], default='MLP')
+    parser.add_argument('--med_code_topk', type=int, default=200)
+    # parser.add_argument('--drug_coding', choices=['rxnorm', 'gpi'], default='rxnorm')
+    # parser.add_argument('--stats', action='store_true')
+    # parser.add_argument('--stats_exit', action='store_true')
+    # Deep PSModels
+    parser.add_argument('--batch_size', type=int, default=256)  # 768)  # 64)
+    parser.add_argument('--learning_rate', type=float, default=1e-3)  # 0.001
+    parser.add_argument('--weight_decay', type=float, default=1e-6)  # )0001)
+    parser.add_argument('--epochs', type=int, default=15)  # 30
+    # LSTM
+    parser.add_argument('--diag_emb_size', type=int, default=128)
+    parser.add_argument('--med_emb_size', type=int, default=128)
+    parser.add_argument('--med_hidden_size', type=int, default=64)
+    parser.add_argument('--diag_hidden_size', type=int, default=64)
+    parser.add_argument('--lstm_hidden_size', type=int, default=100)
+    # MLP
+    # parser.add_argument('--hidden_size', type=str, default='', help=', delimited integers')
+    # Output
+    parser.add_argument('--output_dir', type=str, default='output/')
+
+    args = parser.parse_args()
+
+    # Modifying args
+    args.cuda = torch.cuda.is_available()
+    args.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    if args.random_seed >= 0:
+        rseed = args.random_seed
+    else:
+        from datetime import datetime
+        rseed = datetime.now()
+    args.random_seed = rseed
+    args.save_model_filename = os.path.join(args.output_dir, 'lstm', 'lstm_S{}.model'.format(args.random_seed,
+                                                                                             ))
+    check_and_mkdir(args.save_model_filename)
+    #
+    # args.hidden_size = [int(x.strip()) for x in args.hidden_size.split(',')
+    #                     if (x.strip() not in ('', '0'))]
+    if args.med_code_topk < 1:
+        args.med_code_topk = None
+
+    return args
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    args = parse_args()
+
+    np.random.seed(args.random_seed)
+    torch.manual_seed(args.random_seed)
+    torch.cuda.manual_seed(args.random_seed)
+    random.seed(args.random_seed)
+
+    print('args: ', args)
+    print('random_seed: ', args.random_seed)
+    print('device: ', args.device)
+    # print('Drug {} cohort: '.format(args.treated_drug))
+    # print('save_model_filename', args.save_model_filename)
+
+    encode2namefile = r'dataset/icd9encode2name.pkl'
+    majordatafile = r'dataset/final_pats_1st_neg_triples_{}-{}_new_labeled.pkl'.format('hidd', 'icd9')
+    with open(encode2namefile, 'rb') as f:
+        dx_name = pickle.load(f)
+        print('len(dx_name):', len(dx_name))
+
+    with open(majordatafile, 'rb') as f:
+        data_1st_neg = pickle.load(f)
+        print('len(data_1st_neg):', len(data_1st_neg))
+
+    my_dataset = LSTM_Dataset(data_1st_neg, diag_name=dx_name, diag_code_topk=300)
+    n_feature = my_dataset.DIM_OF_CONFOUNDERS
+    feature_name = my_dataset.FEATURE_NAME
+    print('n_feature: ', n_feature, ':')
+
+    train_ratio = 0.7  # 0.5
+    val_ratio = 0.1
+    print('train_ratio: ', train_ratio,
+          'val_ratio: ', val_ratio,
+          'test_ratio: ', 1 - (train_ratio + val_ratio))
+
+    dataset_size = len(my_dataset)
+    indices = list(range(dataset_size))
+    train_index = int(np.floor(train_ratio * dataset_size))
+    val_index = int(np.floor(val_ratio * dataset_size))
+
+    # data shuffle in here
+    np.random.shuffle(indices)
+
+    train_indices, val_indices, test_indices = indices[:train_index], \
+                                               indices[train_index:train_index + val_index], \
+                                               indices[train_index + val_index:]
+
+    train_sampler = SubsetRandomSampler(train_indices)
+    val_sampler = SubsetRandomSampler(val_indices)
+    test_sampler = SubsetRandomSampler(test_indices)
+
+    train_loader = torch.utils.data.DataLoader(my_dataset, batch_size=256, sampler=train_sampler)
+    val_loader = torch.utils.data.DataLoader(my_dataset, batch_size=256, sampler=val_sampler)
+    test_loader = torch.utils.data.DataLoader(my_dataset, batch_size=256, sampler=test_sampler)
+    data_loader = torch.utils.data.DataLoader(my_dataset, batch_size=256, sampler=SubsetRandomSampler(indices))
+
+    # train_loader_shuffled = torch.utils.data.DataLoader(my_dataset, batch_size=256, sampler=train_sampler)
+    #
+    # print('len(train_loader_shuffled): ', len(train_loader_shuffled),
+    #       'train_loader_shuffled.batch_size: ', train_loader_shuffled.batch_size)
+    #
+    # for confounder, outcome, uid in train_loader_shuffled:
+    #     print(confounder)
+
+    print("**************************************************")
+    print("**************************************************")
+    print('LSTM-Attention PS PSModels learning:')
+
+    paras_grid = {
+        'hidden_size': [32, 64, 128],  # 100
+        'lr': [1e-3],
+        'weight_decay': [1e-6],
+        'batch_size': [256],  # 50
+    }
+    hyper_paras_names, hyper_paras_v = zip(*paras_grid.items())
+    hyper_paras_list = list(itertools.product(*hyper_paras_v))
+    print('Model lstm Searching Space N={}: '.format(len(hyper_paras_list)), paras_grid)
+
+    best_hyper_paras = None
+    best_model = None
+    best_auc = float('-inf')
+    best_balance = float('inf')
+    best_model_epoch = -1
+    best_result_9 = None
+    best_result_95 = None
+    results = []
+    i = -1
+    i_iter = -1
+    for hyper_paras in tqdm(hyper_paras_list):
+        i += 1
+        hidden_size, lr, weight_decay, batch_size = hyper_paras
+        print('In hyper-paras space [{}/{}]...'.format(i, len(hyper_paras_list)))
+        print(hyper_paras_names)
+        print(hyper_paras)
+
+        train_loader_shuffled = torch.utils.data.DataLoader(my_dataset, batch_size=batch_size, sampler=train_sampler)
+        print('len(train_loader_shuffled): ', len(train_loader_shuffled),
+              'train_loader_shuffled.batch_size: ', train_loader_shuffled.batch_size)
+
+        model_params = dict(
+            diag_hidden_size=args.diag_hidden_size,  # 64
+            hidden_size=hidden_size,  # 100,
+            bidirectional=True,
+            diag_vocab_size=len(my_dataset.diag_code_vocab),
+            diag_embedding_size=args.diag_emb_size,  # 128
+            # end_index=my_dataset.diag_code_vocab[CodeVocab.END_CODE],
+            # pad_index=my_dataset.diag_code_vocab[CodeVocab.PAD_CODE],
+        )
+        print('Model: LSTM')
+        print(model_params)
+        model = lstm.LSTMModel(**model_params)
+        if args.cuda:
+            model = model.to('cuda')
+        print(model)
+
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+
+        for epoch in tqdm(range(args.epochs)):
+            i_iter += 1
+            epoch_losses_ipw = []
+            for confounder, labels, outcome, uid in train_loader_shuffled:
+                model.train()
+                # train IPW
+                optimizer.zero_grad()
+
+                if args.cuda:  # confounder = (diag, med, sex, age)
+                    for ii in range(len(confounder)):
+                        confounder[ii] = confounder[ii].to('cuda')
+                    flag = labels.to('cuda').float()
+
+                treatment_logits, _ = model(confounder)
+                loss_ipw = F.binary_cross_entropy_with_logits(treatment_logits, flag)
+
+                loss_ipw.backward()
+                optimizer.step()
+                epoch_losses_ipw.append(loss_ipw.item())
+
+            # just finish 1 epoch
+            # scheduler.step()
+            epoch_losses_ipw = np.mean(epoch_losses_ipw)
+
+            auc_val, loss_val, Y_val, Y_pred_val, uid_val = transfer_data(model, val_loader, cuda=args.cuda,
+                                                                          normalized=False)
+
+            auc_test, loss_test, Y_test, Y_pred_test, uid_test = transfer_data(model, test_loader, cuda=args.cuda,
+                                                                               normalized=False)
+
+            results.append((i_iter, i, epoch, hyper_paras, auc_val, auc_test))
+
+            print('HP-i:{}, epoch:{}, loss:{}, val-auc:{}, test-auc:{}'.format(
+                i, epoch, epoch_losses_ipw, auc_val, auc_test)
+            )
+            print('Validation set, len:', len(Y_val))
+            result_9_val = ml.MLModels._performance_at_specificity_or_threshold(Y_pred_val, Y_val, specificity=0.9)
+            result_95_val = ml.MLModels._performance_at_specificity_or_threshold(Y_pred_val, Y_val, specificity=0.95)
+            print('Test set, len:', len(Y_test))
+            result_9_test = ml.MLModels._performance_at_specificity_or_threshold(Y_pred_test, Y_test, specificity=0.9)
+            result_95_test = ml.MLModels._performance_at_specificity_or_threshold(Y_pred_test, Y_test, specificity=0.95)
+
+            if auc_val > best_auc:
+                best_model = model
+                best_hyper_paras = hyper_paras
+                best_auc = auc_val
+                best_model_epoch = epoch
+                best_result_9 = result_9_val
+                best_result_95 = result_95_val
+                print('Save Best PSModel at Hyper-iter[{}/{}]'.format(i, len(hyper_paras_list)),
+                      'Epoch: ', epoch, 'val-auc:', best_auc, "test-auc", auc_test)
+                print(hyper_paras_names)
+                print(hyper_paras)
+                save_model(model, args.save_model_filename, model_params=model_params)
+
+    col_name = ['i', 'ipara', 'epoch', 'paras', 'val_auc', 'test_auc']
+    results = pd.DataFrame(results, columns=col_name)
+
+    print('Model selection finished! Save Global Best PSModel at Hyper-iter [{}/{}], Epoch: {}'.format(
+        i, len(hyper_paras_list), best_model_epoch), 'val-auc:', best_auc)
+
+    print(hyper_paras_names)
+    print(best_hyper_paras)
+
+    results.to_csv(args.save_model_filename + '_ALL-model-select.csv')
+    auc_test, loss_test, Y_test, Y_pred_test, uid_test = transfer_data(best_model, test_loader, cuda=args.cuda,
+                                                                       normalized=False)
+
+    print('Final results', 'loss_test', loss_test, "test-auc", auc_test)
+
+    result_1 = ml.MLModels._performance_at_specificity_or_threshold(Y_pred_test, Y_test, specificity=0.9)
+    print('......Results at specificity 0.95:')
+    result_2 = ml.MLModels._performance_at_specificity_or_threshold(Y_pred_test, Y_test, specificity=0.95)
+
+    df1 = pd.DataFrame([result_1 + (best_hyper_paras,), result_2 + (best_hyper_paras,)],
+                       columns=["AUC", "threshold", "Specificity", "Sensitivity/recall", "PPV/precision",
+                                "n_negative", "n_positive", "precision_recall_fscore_support", 'best_hyper_paras'],
+                       index=['r_9', 'r_95'])
+    df1.to_csv(os.path.join(os.path.dirname(args.save_model_filename),
+                            'test_results_{}r{}.csv'.format('lstm', args.random_seed)))
+
+    # to dump feature importance, how?
+    # df3 = pd.DataFrame({'train_x_model': model.best_model.coef_[0]}, index=feature_name).reset_index()
+    # df3.to_csv(os.path.join(os.path.dirname(args.save_model_filename),
+    #                         'model_coef_train_LRr{}.csv'.format(args.random_seed)))
+
+    print('Done! Total Time used:', time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)))
